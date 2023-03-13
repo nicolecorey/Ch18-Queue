45faab5908ff01ce125ee71809a15ff8ae9b337d
nicolecorey <122833385+nicolecorey@users.noreply.github.com>
2023-03-12 22:01:38 -0400

    Add files via upload

---
+++ Ch18- rockstarQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue<string> rockstars = new Queue<string>();
+
+            rockstars.Enqueue("Paul McCartney");
+            rockstars.Enqueue("Elvis Presley");
+            rockstars.Enqueue("David Bowie");
+            rockstars.Enqueue("Mick Jagger");
+            rockstars.Enqueue("Jimi Hendrix");
+            
+            Console.WriteLine($"Here are your {rockstars.Count()} rockstars");
+
+            foreach (var m in rockstars)
+            {
+                Console.WriteLine(m);
+            }
+
+            Console.WriteLine(rockstars.Contains("Bon Jovi"));
+            Console.WriteLine("This rockstar is not in line.");
+
+
+            string firstStar = rockstars.Peek();
+            Console.WriteLine($"{firstStar} is the first rockstar to get through the line.");
+            rockstars.Dequeue();
+            
+            Console.WriteLine($"You have {rockstars.Count} rockstars left in the queue");
+
+        }
+    }
+}
